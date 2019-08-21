1715f91cb67911883d7fae6b91b792292828388f
ArtemisVenatrix <williamcrook02@gmail.com>
2019-08-20 19:54:36 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/CampaignForge.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CampaignForge.dir/main.cpp.obj"
+  "CampaignForge.pdb"
+  "CampaignForge.exe"
+  "CampaignForge.exe.manifest"
+  "libCampaignForge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CampaignForge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
